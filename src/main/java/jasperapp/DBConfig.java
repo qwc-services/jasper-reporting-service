57e03bcdc54ffaec8d424ad75fc944b4e2b19e60
edigonzales <edi.gonzales@gmail.com>
2021-12-02 11:13:47 +0100

    do not create datasource if contig is not available

--- src/main/java/jasperapp/DBConfig.java
+++ src/main/java/jasperapp/DBConfig.java
@@ -7,6 +7,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 
 @Configuration
 public class DBConfig {
@@ -14,30 +15,35 @@ public class DBConfig {
     @Bean(name = "dataSource0")
     @Primary
     @ConfigurationProperties("spring.datasource0")
+    @ConditionalOnProperty(prefix = "spring.datasource0", name="jdbc-url", matchIfMissing = false)
     public DataSource dataSource0() {
         return DataSourceBuilder.create().build();
     }
 
     @Bean(name = "dataSource1")
     @ConfigurationProperties("spring.datasource1")
+    @ConditionalOnProperty(prefix = "spring.datasource1", name="jdbc-url", matchIfMissing = false)
     public DataSource dataSource1() {
         return DataSourceBuilder.create().build();
     }
 
     @Bean(name = "dataSource2")
     @ConfigurationProperties("spring.datasource2")
+    @ConditionalOnProperty(prefix = "spring.datasource2", name="jdbc-url", matchIfMissing = false)
     public DataSource dataSource2() {
         return DataSourceBuilder.create().build();
     }
 
     @Bean(name = "dataSource3")
     @ConfigurationProperties("spring.datasource3")
+    @ConditionalOnProperty(prefix = "spring.datasource3", name="jdbc-url", matchIfMissing = false)
     public DataSource dataSource3() {
         return DataSourceBuilder.create().build();
     }
 
     @Bean(name = "dataSource4")
     @ConfigurationProperties("spring.datasource4")
+    @ConditionalOnProperty(prefix = "spring.datasource4", name="jdbc-url", matchIfMissing = false)
     public DataSource dataSource4() {
         return DataSourceBuilder.create().build();
     }
